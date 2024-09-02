30d05891905316053a95aaa8723ae7b90d64a44f
Lucas Pinto <lucasdamianpinto1987@gmail.com>
2024-09-02 17:48:40 +0200

    New changes in app.js

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
-const express = require ('express')
-const app = express()
+const express = require('express');
+const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
 
 let usuarios = [
     { id: 1, nombre: 'Ryu', edad: 32, lugarProcedencia: 'Japón' },
@@ -21,13 +20,30 @@ app.get('/', (req, res) => {
             `<li> id: ${usuario.id} | nombre: ${usuario.nombre}</li>`).join('')
         }
         </ul>
-        <form>
-        <label></label>
-        <input></input>
-        <button></button>
-        </form>`)
+        <form action="/usuarios" method="POST">
+            <label for='nombre'>Nombre</label>
+            <input type='text' id='nombre' name='nombre' required>
+            <label for='edad'>Edad</label>
+            <input type='number' id='edad' name='edad' required>
+            <label for='lugarProcedencia'>Lugar de Procedencia</label>
+            <input type='text' id='lugarProcedencia' name='lugarProcedencia' required>
+            <button type='submit'>Agregar usuario</button>
+        </form>`
+    );
 });
 
-app.listen(3000, () => {
-    console.log('Express esta escuchando en el puerto 3000')
-})
+app.post('/usuarios', (req, res) => {
+    const nuevoUsuario = {
+        id: usuarios.length + 1,
+        nombre: req.body.nombre,
+        edad: req.body.edad,
+        lugarProcedencia: req.body.lugarProcedencia,
+    };
+    usuarios.push(nuevoUsuario);
+    res.redirect('/');
+});
+
+const PORT = 3000
+app.listen(PORT, () => {
+  console.log(`El servidor está escuchando en el puerto http://localhost:${PORT}`)
+});
